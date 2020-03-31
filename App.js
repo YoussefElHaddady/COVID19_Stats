c2a225fb973fd6d67b7747131e0b749be80cd8e1
YouSsef <youssef.elhaddady@usmba.ac.ma>
2020-03-31 17:54:52 +0200

    fetching data from api and display in flatList (search included)

--- App.js
+++ App.js
@@ -1,114 +1,137 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
+import {StyleSheet, View, ActivityIndicator, FlatList} from 'react-native';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
+  Container,
   Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  Item,
+  Input,
+  Icon,
+  List,
+  ListItem,
+  Left,
+  Body,
+  Right,
+  Thumbnail,
+  Text,
+} from 'native-base';
+import _ from 'lodash';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      data: [],
+      fullData: [],
+      error: null,
+      query: '',
+      // timeLaps: 1,
+    };
+  }
+
+  componentDidMount() {
+    // this.interval = setInterval(() => {
+    this.loadingData();
+    //   }, this.state.timeLaps * 60 * 1000);
+  }
+
+  // componentWillUnmount() {
+  //   clearInterval(this.interval);
+  // }
+
+  loadingData = _.debounce(() => {
+    console.log('calling loadingData');
+    const apiURL = 'https://corona.lmao.ninja/countries';
+    return fetch(apiURL)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          isLoading: false,
+          data: responseJson,
+          fullData: responseJson,
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error: error,
+          isLoading: false,
+        });
+      });
+  }, 250);
+
+  _renderFooter = () => {
+    if (!this.state.isLoading) return null;
+    return (
+      <View style={styles.indicator}>
+        <ActivityIndicator animating size="large" />
+      </View>
+    );
+  };
+
+  _renderItem = ({item, index}) => {
+    return (
+      <ListItem avatar onPress={() => alert(item.country)}>
+        <Left>
+          <Thumbnail source={{uri: item.countryInfo.flag}} />
+        </Left>
+        <Body>
+          <Text>{item.country}</Text>
+          <Text note>cases : {item.cases}</Text>
+          <Text note>todayCases : {item.todayCases}</Text>
+        </Body>
+        {/* <Right>
+          <Text note>3:43 pm</Text>
+        </Right> */}
+      </ListItem>
+    );
+  };
+
+  handleSearch = text => {
+    const formattedQuery = text.toLowerCase();
+    const data = _.filter(this.state.fullData, countries => {
+      if (countries.country.toLowerCase().includes(formattedQuery)) {
+        return true;
+      }
+      return false;
+    });
+    this.setState({
+      data: data,
+      query: text,
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Header searchBar rounded>
+          <Item>
+            {/* <Icon name="ios-search" /> */}
+            <Input placeholder="Search" onChangeText={this.handleSearch} />
+          </Item>
+        </Header>
+        <List>
+          <FlatList
+            data={this.state.data}
+            renderItem={this._renderItem}
+            keyExtractor={(item, index) => index.toString()}
+            ListFooterComponent={this._renderFooter}
+          />
+        </List>
+      </Container>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    backgroundColor: '#f00',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  indicator: {
+    paddingVertical: 20,
+    borderTopWidth: 1,
+    borderColor: '#CED0CE',
   },
 });
-
-export default App;
